64262a057e9142723c21f5f7327aed2331d31de7
sod-04 <sara.ojeda.ttv.995@unilibre.edu.co>
2024-11-07 17:06:29 -0500

    Actualización de la fecha de devolución de un préstamo

--- laboratorio-5_parte-2.sql
+++ laboratorio-5_parte-2.sql
@@ -4,3 +4,10 @@ USE biblioteca;
 UPDATE libros
 SET Disponible = FALSE
 WHERE ID_libro = 2;
+
+--------- Actualizar la fecha de devolución de un préstamo.
+
+USE biblioteca;
+UPDATE prestamos
+SET fecha_devolucion = '2024-09-01'
+WHERE ID_prestamo = 2;
