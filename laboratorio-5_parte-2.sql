a06d6e61c64b3dd5d7ef9c47ec680c9a6411b815
sod-04 <sara.ojeda.ttv.995@unilibre.edu.co>
2024-11-07 15:28:58 -0500

    Creación tabla autores

--- laboratorio-5_parte-2.sql
+++ laboratorio-5_parte-2.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE Biblioteca;
+
+USE biblioteca;
+CREATE TABLE autores (
+ID_autor INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+Nombre VARCHAR (100) NOT NULL,
+Nacionalidad VARCHAR (100) NOT NULL,
+Fecha_nacimiento DATE
+);
+
